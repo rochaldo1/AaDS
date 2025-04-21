cd71faccd6721be80e5478d714dcb1a43b16a48a
rochaldo1 <xusainow.artur2018@yandex.ru>
2025-04-21 15:16:27 +0700

    revamped LCS

--- AaDS_1/LCS.cs
+++ AaDS_1/LCS.cs
@@ -48,24 +48,35 @@
         {
             int m = a.Length;
             int n = b.Length;
-            int[,] dp = new int[2, n + 1];
+
+            int[,] dp = new int[m + 1, n + 1];
 
             for (int i = 0; i <= m; i++)
             {
                 for (int j = 0; j <= n; j++)
                 {
                     if (i == 0 || j == 0)
-                        dp[i % 2, j] = 0;
+                    {
+                        dp[i, j] = 0;
+                    }
                     else if (a[i - 1] == b[j - 1])
-                        dp[i % 2, j] = dp[(i - 1) % 2, j - 1] + 1;
+                    {
+                        dp[i, j] = dp[i - 1, j - 1] + 1;
+                    }
                     else
-                        dp[i % 2, j] = Math.Max(dp[(i - 1) % 2, j], dp[i % 2, j - 1]);
+                    {
+                        dp[i, j] = Math.Max(dp[i - 1, j], dp[i, j - 1]);
+                    }
                 }
             }
 
             int[] result = new int[n + 1];
+
             for (int j = 0; j <= n; j++)
-                result[j] = dp[m % 2, j];
+            {
+                result[j] = dp[m, j];
+            }
+
             return result;
         }
 
--- AaDS_1/Program.cs
+++ AaDS_1/Program.cs
@@ -1,14 +1,25 @@
 ﻿using AaDS_1;
+using System.Diagnostics;
 
 public static class Program
 {
     private static void Main(string[] args)
     {
+        Stopwatch sw = new();
+        TimeSpan elapsed = new();
+
         string a = "ABCBABD";
         string b = "BDCABA";
 
+        sw.Start();
         string lcs = LCS.FindLCS(a, b);
+        sw.Stop();
+        elapsed = sw.Elapsed;
+
         Console.WriteLine($"LCS для {a} и {b}: {lcs}");
         Console.WriteLine($"Длина LCS: {lcs.Length}");
+        Console.WriteLine($"Время поиска: {elapsed.TotalMilliseconds} м.сек.");
+        Console.WriteLine();
+        sw.Restart();
     }
 }
