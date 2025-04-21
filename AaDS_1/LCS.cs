f10bb86ec34f95132d8e63d7dfa253aee7392f19
rochaldo1 <xusainow.artur2018@yandex.ru>
2025-04-21 13:57:41 +0700

    Hirschberg method

---
+++ AaDS_1/LCS.cs
@@ -0,0 +1,79 @@
+﻿namespace AaDS_1
+{
+    public class LCS
+    {
+        public static string FindLCS(string a, string b)
+        {
+            return Hirschberg(a, b, a.Length, b.Length);
+        }
+
+        private static string Hirschberg(string a, string b, int m, int n)
+        {
+            if (n == 0)
+                return "";
+            if (m == 1)
+            {
+                for (int j = 0; j < n; j++)
+                    if (b[j] == a[0])
+                        return a[0].ToString();
+                return "";
+            }
+
+            int mid = m / 2;
+            string aFirstHalf = a.Substring(0, mid);
+            string aSecondHalf = a.Substring(mid);
+
+            int[] l1 = ComputeLCSLength(aFirstHalf, b);
+            int[] l2 = ComputeLCSLength(ReverseString(aSecondHalf), ReverseString(b));
+
+            int max = 0;
+            int k = 0;
+            for (int j = 0; j <= n; j++)
+            {
+                int sum = l1[j] + l2[n - j];
+                if (sum > max)
+                {
+                    max = sum;
+                    k = j;
+                }
+            }
+
+            string lcsFirstHalf = Hirschberg(aFirstHalf, b.Substring(0, k), mid, k);
+            string lcsSecondHalf = Hirschberg(aSecondHalf, b.Substring(k), m - mid, n - k);
+
+            return lcsFirstHalf + lcsSecondHalf;
+        }
+
+        private static int[] ComputeLCSLength(string a, string b)
+        {
+            int m = a.Length;
+            int n = b.Length;
+            int[,] dp = new int[2, n + 1];
+
+            for (int i = 0; i <= m; i++)
+            {
+                for (int j = 0; j <= n; j++)
+                {
+                    if (i == 0 || j == 0)
+                        dp[i % 2, j] = 0;
+                    else if (a[i - 1] == b[j - 1])
+                        dp[i % 2, j] = dp[(i - 1) % 2, j - 1] + 1;
+                    else
+                        dp[i % 2, j] = Math.Max(dp[(i - 1) % 2, j], dp[i % 2, j - 1]);
+                }
+            }
+
+            int[] result = new int[n + 1];
+            for (int j = 0; j <= n; j++)
+                result[j] = dp[m % 2, j];
+            return result;
+        }
+
+        private static string ReverseString(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+    }
+}
--- AaDS_1/Program.cs
+++ AaDS_1/Program.cs
@@ -1,7 +1,14 @@
-﻿public static class Program
+﻿using AaDS_1;
+
+public static class Program
 {
     private static void Main(string[] args)
     {
-        
+        string a = "ABCBABD";
+        string b = "BDCABA";
+
+        string lcs = LCS.FindLCS(a, b);
+        Console.WriteLine($"LCS для {a} и {b}: {lcs}");
+        Console.WriteLine($"Длина LCS: {lcs.Length}");
     }
 }
